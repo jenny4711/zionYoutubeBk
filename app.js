92f0a12551ae2a0a7fe975b68cf7d9d103b95bce
jiylee <jjenny4711@gmail.com>
2024-02-17 20:05:51 -0500

    remove console.log for key

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/zs',indexRoutes)
-console.log(process.env.OPENAI_API_KEY1,'env')
-//google ai2 :AIzaSyBNttB6_g1eWPoTgM2P4MwA86jjvk9rfwI
+
+
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 console.log(mongoURI,'mongouri')
@@ -24,5 +24,4 @@ mongoose
     console.log('server on');
   })
 
-//   # GOOGLE_API_KEY="AIzaSyBPMxXnFVWTUjTj6scSo-oG2H2iLWJS2CY"
-// # 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
+
