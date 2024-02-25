cb263541c8f7b78253356c52a83c27d3c66e37d2
jiylee <jjenny4711@gmail.com>
2024-02-24 19:19:48 -0500

    test

--- controllers/history.controller.js
+++ controllers/history.controller.js
@@ -1,163 +1,79 @@
-// const{ translateResult ,createChatWithGoogle } =require('../utils/ai') ;
-// const History = require('../model/history')
-// const historyController={};
-// const{ YoutubeTranscript} = require('youtube-transcript') ;
-// const userController = require('./user.controller');
-
-//  async function saveSummary({videoId,summaryORG,lang,ask,summary}){
-//   try{
-//     const video=await History.findOne({videoId,lang,ask});
-//     console.log(ask,'ask-saveSummary!!!!!!!')
-//     if(!video){
-//       const newHistory = new History({
-//         videoId,
-//         summaryORG,
-//         lang,
-//         ask,
-//         summary,
-//       })
-
-//       await newHistory.save()
-//       console.log('save!')
-//     }else{
-      
-//       console.log('already have it','video!!')
-//     }
-  
-//   }catch(error){
-//     console.log(error,'error-saveSummary')
-//   }
-// }
-
-
-// historyController.makeSummary=async (req,res)=>{
-//   try{
-//   const {videoId,lang,ask}=req.body;
-//   console.log(ask,'ask-makeSummary!!!!!!!!!')
-//     const textes=[]
-
-//     let findVideo = await History.findOne({videoId,lang,ask});
-
-//     if(!videoId){
-//       return res.status(400).json({message:'VideoId is required'})
-//     }
-// if(!findVideo){
-//   console.log(findVideo,'findVideo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-//   const transcript = await YoutubeTranscript.fetchTranscript(videoId)
-//   if (!transcript || !Array.isArray(transcript)) throw new Error("Couldn't provide the Trancript!")
-//   transcript.map((item)=>{
-//     textes.push(item.text)
-//   })
-// console.log(textes,'textes!')
-//  let summaryORG = await createChatWithGoogle(textes,ask)
-//  let summary = await translateResult(summaryORG,lang)
-
-//  if(summaryORG == "" || summary == ""){
-// throw new Error("Ai couldn't read summary. Please try again later!")
-
-//  }
-//  await saveSummary({videoId,summaryORG,lang,ask,summary})
-//  console.log(summaryORG ,'summaryORG!')
-//  console.log(summary,'summary')
-//  res.status(200).json({data:summary,videoId:videoId})
-
-
-// }else{
-//   let summary=findVideo.summary
-//   let videoId=findVideo.videoId
-//   console.log(summary,'summary-makeSummary!already have it')
-//   return res.status(200).json({data:summary,videoId:videoId})
-// }
-
-//   }catch(error){
-//     console.log(error,'errorMakeSummary!!!!!')
-//     res.status(500).json({ message: 'Failed to get transcript', error: error.message });
-//   }
-// }
-
-
-// module.exports=historyController
-
-const { translateResult, createChatWithGoogle } = require('../utils/ai');
-const History = require('../model/history');
-const historyController = {};
-const { YoutubeTranscript } = require('youtube-transcript');
+const{ translateResult ,createChatWithGoogle } =require('../utils/ai') ;
+const History = require('../model/history')
+const historyController={};
+const{ YoutubeTranscript} = require('youtube-transcript') ;
 const userController = require('./user.controller');
 
-async function saveSummary({ videoId, summaryORG, lang, ask, summary }) {
-    try {
-        const video = await History.findOne({ videoId, lang, ask });
-        console.log(ask, 'ask-saveSummary!!!!!!!');
-        if (!video) {
-            const newHistory = new History({
-                videoId,
-                summaryORG,
-                lang,
-                ask,
-                summary,
-            });
+ async function saveSummary({videoId,summaryORG,lang,ask,summary}){
+  try{
+    const video=await History.findOne({videoId,lang,ask});
+    console.log(ask,'ask-saveSummary!!!!!!!')
+    if(!video){
+      const newHistory = new History({
+        videoId,
+        summaryORG,
+        lang,
+        ask,
+        summary,
+      })
 
-            await newHistory.save();
-            console.log('save!');
-        } else {
-            console.log('already have it', 'video!!');
-        }
-    } catch (error) {
-        console.error('Error in saveSummary:', error);
+      await newHistory.save()
+      console.log('save!')
+    }else{
+      
+      console.log('already have it','video!!')
     }
+  
+  }catch(error){
+    console.log(error,'error-saveSummary')
+  }
 }
 
-historyController.makeSummary = async (req, res) => {
-    try {
-        const { videoId, lang, ask } = req.body;
-        console.log(ask, 'ask-makeSummary!!!!!!!!!');
-        const textes = [];
 
-        let findVideo = await History.findOne({ videoId, lang, ask });
+historyController.makeSummary=async (req,res)=>{
+  try{
+  const {videoId,lang,ask}=req.body;
+  console.log(ask,'ask-makeSummary!!!!!!!!!')
+    const textes=[]
 
-        if (!videoId) {
-            return res.status(400).json({ message: 'VideoId is required' });
-        }
-        if (!findVideo) {
-            console.log(findVideo, 'findVideo');
-            let transcript;
-            try {
-                transcript = await YoutubeTranscript.fetchTranscript(videoId);
-            } catch (error) {
-                console.error('Failed to fetch transcript:', error);
-                return res.status(500).json({ message: "Couldn't fetch transcript", error: error.message });
-            }
-            
-            if (!transcript || !Array.isArray(transcript)) {
-                return res.status(500).json({ message: "Transcript format is invalid or empty" });
-            }
-            
-            transcript.forEach((item) => {
-                textes.push(item.text);
-            });
-            console.log(textes, 'textes!');
+    let findVideo = await History.findOne({videoId,lang,ask});
 
-            let summaryORG = await createChatWithGoogle(textes.join(' '), ask);
-            let summary = await translateResult(summaryORG, lang);
-
-            if (summaryORG === "" || summary === "") {
-                return res.status(500).json({ message: "AI couldn't generate a summary. Please try again later!" });
-            }
-
-            await saveSummary({ videoId, summaryORG, lang, ask, summary });
-            console.log(summaryORG, 'summaryORG!');
-            console.log(summary, 'summary');
-            res.status(200).json({ data: summary, videoId: videoId });
-        } else {
-            let summary = findVideo.summary;
-            let videoId = findVideo.videoId;
-            console.log(summary, 'summary-makeSummary!already have it');
-            return res.status(200).json({ data: summary, videoId: videoId });
-        }
-    } catch (error) {
-        console.error('Error in makeSummary:', error);
-        res.status(500).json({ message: 'Failed to generate summary', error: error.message });
+    if(!videoId){
+      return res.status(400).json({message:'VideoId is required'})
     }
+if(!findVideo){
+  console.log(findVideo,'findVideo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId)
+  if (!transcript || !Array.isArray(transcript)) throw new Error("Couldn't provide the Trancript!")
+  transcript.map((item)=>{
+    textes.push(item.text)
+  })
+console.log(textes,'textes!')
+ let summaryORG = await createChatWithGoogle(textes,ask)
+ let summary = await translateResult(summaryORG,lang)
+
+ if(summaryORG == "" || summary == ""){
+throw new Error("Ai couldn't read summary. Please try again later!")
+
+ }
+ await saveSummary({videoId,summaryORG,lang,ask,summary})
+ console.log(summaryORG ,'summaryORG!')
+ console.log(summary,'summary')
+ res.status(200).json({data:summary,videoId:videoId})
+
+
+}else{
+  let summary=findVideo.summary
+  let videoId=findVideo.videoId
+  console.log(summary,'summary-makeSummary!already have it')
+  return res.status(200).json({data:summary,videoId:videoId})
 }
 
-module.exports = historyController;
+  }catch(error){
+    console.log(error,'errorMakeSummary!!!!!')
+    res.status(500).json({ message: 'Failed to get transcript', error: error.message });
+  }
+}
+
+
+module.exports=historyController
