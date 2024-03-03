e39959a6a72f14badf620a8633199c6045c6c566
jiylee <jjenny4711@gmail.com>
2024-03-02 23:44:31 -0500

    add safty_setting

--- utils/ai.js
+++ utils/ai.js
@@ -1,7 +1,7 @@
 //Summarize the following into two sentences at the 12th grade level:
 //방법-Summeraize following in a step-by-step format,providing clear instructions on how to complete a task or achieve a goal.
 //스토리형식-Summarize following in a narrative format, capturing the key events, characters, and plot points in a concise and engaging way.
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI ,HarmBlockThreshold,HarmCategory} = require("@google/generative-ai");
 const OpenAI = require('openai');
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
@@ -12,9 +12,29 @@ const openai = new OpenAI({
 });
 
 const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
+const safetySettings = [
+  {
+  category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+  threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+  category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+  threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+  category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+  threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+  category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+  threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  ];
+  
+  
 
 const createChatWithGoogle = async (prompt, ask) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"},safetySettings );
 console.log(prompt,'askCreateChatWithGoogle')
   //------------------------------------------------------------------------------------------------
   const result = await model.generateContentStream(`${ask}  ${prompt} `);
