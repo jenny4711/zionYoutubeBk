7718cb107c79960c6830de2d8cd8c014525aefa7
jiylee <jjenny4711@gmail.com>
2024-03-14 17:39:47 -0400

    ddd

--- utils/ai.js
+++ utils/ai.js
@@ -15,19 +15,19 @@ const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 const safetySettings = [
   {
   category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-  threshold: HarmBlockThreshold.BLOCK_NONE,
+  threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   {
   category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-  threshold: HarmBlockThreshold.BLOCK_NONE,
+  threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   {
   category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-  threshold: HarmBlockThreshold.BLOCK_NONE,
+  threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   {
   category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-  threshold: HarmBlockThreshold.BLOCK_NONE,
+  threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   ];
 
